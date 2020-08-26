105cc8b52b832dacc918262b5ec3a812f75794df
sawarad <sawarad28@gmail.com>
2020-08-26 16:53:57 +0300

    - issue #2 fixed

--- src/ColorEntry.py
+++ src/ColorEntry.py
@@ -4,34 +4,36 @@ import tkinter as tk
 class ColorEntry(tk.Frame):
     CANVAS_SIZE = 20
     entry_var = None
-
+    
     def __init__(self, parent, label, default):
         super().__init__(parent)
+        self.observers = []
         self.label = tk.Label(self, text=label)
         self.entry_var = tk.StringVar()
         self.entry = tk.Entry(
-                self,
-                textvariable=self.entry_var,
-                width=10
+            self,
+            textvariable=self.entry_var,
+            width=10
         )
         self.canvas = tk.Canvas(
-                self,
-                width=self.CANVAS_SIZE,
-                height=self.CANVAS_SIZE
+            self,
+            width=self.CANVAS_SIZE,
+            height=self.CANVAS_SIZE
         )
         self.entry_var.trace('w', self.update_canvas)
         self.entry_var.set(default)
-
+        self.valid = None
+        
         self.label.pack(side=tk.LEFT)
         self.canvas.pack(side=tk.RIGHT)
         self.entry.pack(side=tk.RIGHT)
-
+    
     def update_canvas(self, *args):
         try:
             self.canvas.create_rectangle(
-                    0, 0,
-                    self.CANVAS_SIZE, self.CANVAS_SIZE,
-                    fill=self.entry_var.get()
+                0, 0,
+                self.CANVAS_SIZE, self.CANVAS_SIZE,
+                fill=self.entry_var.get()
             )
             self.entry.config(fg='black')
             self.valid = True
@@ -41,10 +43,21 @@ class ColorEntry(tk.Frame):
                 self.entry.config(fg='red')
             else:
                 raise e
-
+        
+        if not self.entry_var.get():
+            self.valid = False
+        self.notify_all()
+    
     def set_label(self, label):
         self.label.config(text=label)
-
+    
     @property
     def color(self):
         return self.entry.get()
+    
+    def add_observer(self, observer):
+        self.observers.append(observer)
+    
+    def notify_all(self):
+        for observer in self.observers:
+            observer.check_valid(self)
--- src/SaveButton.py
+++ src/SaveButton.py
@@ -8,3 +8,16 @@ class SaveButton(tk.Button):
                 command=lambda: parent.master.save_to_file(parent.entry_var.get()),
                 **options
         )
+        self.notifiers = dict()
+    
+    def check_valid(self, notifier):
+        """
+        if all of notifiers are valid then button activates
+        :param notifier: must have valid field
+        """
+        self.notifiers[notifier] = notifier.valid
+        
+        if all(self.notifiers.values()):
+            self['state'] = 'active'
+        else:
+            self['state'] = 'disabled'
